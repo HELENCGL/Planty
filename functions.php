fa3cc9943bb4161c5bc2f14b78d54c851fa4668a
Helene <hcheynet@orange.fr>
2024-03-21 21:21:25 +0100

    Centrer le bouton du formulaire

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles() {
 	wp_enqueue_style('parent', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('child', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
