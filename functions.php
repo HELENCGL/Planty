853401d2d8ba5c6ac0fa6cff0c9c20348a4f5797
Helene <hcheynet@orange.fr>
2024-03-29 19:00:14 +0100

    Paramétrage des produits de la page commande

--- functions.php
+++ functions.php
@@ -1,7 +1,24 @@
 <?php
-/* enqueue script for parent theme stylesheeet */        
+/* enqueue script for parent theme stylesheeet */
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-function my_theme_enqueue_styles() {
+function my_theme_enqueue_styles()
+{
 	wp_enqueue_style('parent', get_template_directory_uri() . '/style.css');
 	wp_enqueue_style('child', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+
+// Ajout d'un hook pour le menu personnalisé
+add_filter('wp_nav_menu_items', 'menu_with_admin', 10, 2);
+
+function menu_with_admin($items, $args)
+{
+
+	if (is_user_logged_in() && $args->theme_location == 'main_menu') {
+		$items .= '<li class="menu-survol"><a href="http://localhost/planty/wp-admin">Admin</a>';
+	}
+	$items .= '</li><li class="menu-button"><a href="http://localhost/planty/precommande"><span>Commande</span></a></li>';
+
+
+	return $items;
+
 }
